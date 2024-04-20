03fb73ff01123f16e0f3988d19d264ce89c834a9
Carlos Ramirez <carlos@extrasistemas.com>
2024-04-20 12:39:20 -0600

    feat: add envirnomet STRIPE_SECRET with my apikey and add stripe in them PaymentService

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('create-session-payment')
+  createPaymentSession() {
+    return 'payment session created';
+  }
+
+  @Get('success')
+  success() {
+    return {
+      ok: true,
+      message: 'Payment successful',
+    };
+  }
+
+  @Get('cancel')
+  cancel() {
+    return {
+      ok: true,
+      message: 'Payment Cenceled',
+    };
+  }
+
+  @Post('hook')
+  stripeHook() {
+    return {
+      ok: true,
+      message: 'Payment Hook',
+    };
+  }
 }
